22815c5342aa25eb42085bc8dd8a799d2f456438
ciscom224 <mamadouciss224e@gmail.com>
2024-05-09 02:47:53 +0200

    MAJ

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { applyMiddleware, createStore, compose } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
 import { thunk } from 'redux-thunk';
 import rootReducer from './reducers/reducers';
 import App from './pages/App';
@@ -11,7 +10,7 @@ import App from './pages/App';
 import { getUsers } from './actions/users.actions';
 import './index.css';
 // Redux store
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = compose;
 const store = createStore(
   rootReducer,
   composeEnhancers(applyMiddleware(thunk))
@@ -23,8 +22,6 @@ store.dispatch(getUsers());
 const root = createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-      <React.StrictMode>
         <App />
-      </React.StrictMode>
   </Provider>
 );
